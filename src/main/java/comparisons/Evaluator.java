5d52701718230f268607c97b67b6bceda4bbe004
Paulina Ksienzyk <pilnicka8@gmail.com>
2020-12-19 13:17:09 +0100

    Delete enum Grade

--- src/main/java/comparisons/Evaluator.java
+++ src/main/java/comparisons/Evaluator.java
@@ -4,24 +4,4 @@ public class Evaluator {
 
 
 
-
-
-    enum GRADE {
-        FIVE(5),
-        FOUR(4),
-        THREE(3),
-        TWO(2);
-
-        private final int gradeNr;
-
-        GRADE(int i) {
-        this.gradeNr = i;
-        }
-
-
-        @Override
-        public String toString() {
-            return String.valueOf(gradeNr);
-        }
-    }
 }
