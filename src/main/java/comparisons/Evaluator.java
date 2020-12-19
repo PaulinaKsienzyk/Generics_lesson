31b2a8fbed6e7101b95c4bf6ee97d4bda54f968d
Paulina Ksienzyk <pilnicka8@gmail.com>
2020-12-19 13:04:10 +0100

    Create constants - grades

--- src/main/java/comparisons/Evaluator.java
+++ src/main/java/comparisons/Evaluator.java
@@ -1,4 +1,27 @@
 package comparisons;
 
 public class Evaluator {
+
+
+
+
+
+    enum GRADE {
+        FIVE(5),
+        FOUR(4),
+        THREE(3),
+        TWO(2);
+
+        private final int gradeNr;
+
+        GRADE(int i) {
+        this.gradeNr = i;
+        }
+
+
+        @Override
+        public String toString() {
+            return String.valueOf(gradeNr);
+        }
+    }
 }
