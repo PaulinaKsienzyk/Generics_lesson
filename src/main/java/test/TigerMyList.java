3f8fcc6f3e2e9818b0e41498cbae6357011f4c71
Paulina Ksienzyk <pilnicka8@gmail.com>
2020-12-19 12:40:16 +0100

    Generics in 2004

---
+++ src/main/java/test/TigerMyList.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TigerMyList {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("Lukasz S.");
+        list.add("Lukasz B.");
+        list.add("Lukasz P.");
+
+        System.out.println(list);
+        System.out.println(list.get(0));
+
+//        String name2 = list.get(2);         // ynocompile
+//        String name2 = (String) list.get(2);
+        String name3 = "test " + list.get(2);
+
+        System.out.println(name3);
+
+//        list.add(LocalDateTime.now());        // finally we can not put it in
+        System.out.println(list);
+
+        List<Integer> shoeSizes = new ArrayList<Integer>();
+//        shoeSizes.add("one");
+        shoeSizes.add(38);
+        shoeSizes.add(42);
+
+        for (Integer size : shoeSizes) {
+            if (size == 42) {
+                System.out.println("It contains the meaning of life");
+            } else {
+                System.out.println("It does not contain the meaning of life");
+            }
+        }
+
+    }
+}
