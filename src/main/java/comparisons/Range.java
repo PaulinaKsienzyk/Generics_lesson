13737dba66b21ac61ed8fa08bbd492347586b60d
Paulina Ksienzyk <pilnicka8@gmail.com>
2020-12-19 13:19:54 +0100

    Add bounds

--- src/main/java/comparisons/Range.java
+++ src/main/java/comparisons/Range.java
@@ -1,4 +1,14 @@
 package comparisons;
 
 public class Range {
+
+    int bottomBound;
+    int upperBound;
+
+    public Range(int bottomBound, int upperBound) {
+        this.bottomBound = bottomBound;
+        this.upperBound = upperBound;
+    }
+
+
 }
