bb3eae34c2dd3c1f209a8de5ec20c3cf39976bb3
Paulina Ksienzyk <pilnicka8@gmail.com>
2020-12-20 22:44:34 +0100

    Make it possible to write different names of files

--- src/main/java/comparisons/PreAcademy/CVSWriter.java
+++ src/main/java/comparisons/PreAcademy/CVSWriter.java
@@ -8,13 +8,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class CVSWriter {
+public class CVSWriter implements AutoCloseable{
 
     public static void main(String[] args) throws IOException {
         List<PreAcademyStudent> students = new ArrayList<>();
         PreAcademyStudent s1 = new PreAcademyStudent("Paulina Ksienzyk", 5, 10, 15);
         PreAcademyStudent s2 = new PreAcademyStudent("Rafał Mazurek", 17, 10, 7);
-        PreAcademyStudent s3 = new PreAcademyStudent("Anna Ignacy" , 5, 20, 3);
+        PreAcademyStudent s3 = new PreAcademyStudent("Anna Ignacy", 5, 20, 3);
         students.add(s1);
         students.add(s2);
         students.add(s3);
@@ -30,7 +30,7 @@ public class CVSWriter {
         }
 
         CVSWriter cvsWriter = new CVSWriter();
-        cvsWriter.givenDataArray_whenConvertToCSV_thenOutputCreated(data);
+        cvsWriter.givenDataArray_whenConvertToCSV_thenOutputCreated(data, "preAcademyStudents.csv");
 
     }
 
@@ -49,13 +49,18 @@ public class CVSWriter {
         return escapedData;
     }
 
-    public void givenDataArray_whenConvertToCSV_thenOutputCreated(List<String[]> dataLines) throws IOException {
-        File csvOutputFile = new File("preAcademyStudents.csv");
+    public void givenDataArray_whenConvertToCSV_thenOutputCreated(List<String[]> dataLines, String fileName) throws IOException {
+        File csvOutputFile = new File(fileName);
         try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
             dataLines.stream()
                     .map(this::convertToCSV)
                     .forEach(pw::println);
         }
         assert (csvOutputFile.exists());
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing!");
     }
 }
