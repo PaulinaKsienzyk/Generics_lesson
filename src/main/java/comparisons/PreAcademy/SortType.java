28653910d5cdf07d0e4913ad3a2c07293bb7c59a
Paulina Ksienzyk <pilnicka8@gmail.com>
2020-12-20 22:44:51 +0100

    Delete nrs from sorted types

--- src/main/java/comparisons/PreAcademy/SortType.java
+++ src/main/java/comparisons/PreAcademy/SortType.java
@@ -2,22 +2,12 @@ package comparisons.PreAcademy;
 
 public enum SortType {
 
-    TOTAL_DESC(0),
-    TOTAL_ASC(1),
-    QUIZZES_DESC(2),
-    QUIZZES_ASC(3),
-    TASKS_DESC(4),
-    TASKS_ASC(5),
-    ACTIVITY_DESC(5),
-    ACTIVITY_ASC(6);
-
-    private int nr;
-
-    SortType(int nr) {
-        this.nr = nr;
-    }
-
-    public int getNr() {
-        return nr;
-    }
+    TOTAL_DESC,
+    TOTAL_ASC,
+    QUIZZES_DESC,
+    QUIZZES_ASC,
+    TASKS_DESC,
+    TASKS_ASC,
+    ACTIVITY_DESC,
+    ACTIVITY_ASC;
 }
