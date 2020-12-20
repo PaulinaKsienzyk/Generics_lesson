a37dfe1aa081336283bd2528bde51b7ab912fcc7
Paulina Ksienzyk <pilnicka8@gmail.com>
2020-12-20 22:45:17 +0100

    Change sortings

--- src/main/java/comparisons/PreAcademy/StudentsBinder.java
+++ src/main/java/comparisons/PreAcademy/StudentsBinder.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+
+import static comparisons.PreAcademy.SortType.*;
 
 public class StudentsBinder {
 
@@ -16,28 +19,59 @@ public class StudentsBinder {
         this.sortType = sortType;
     }
 
-    // sort using Comparable
-    private List<PreAcademyStudent> sortUsingTotalPoints() {
-        List<PreAcademyStudent> sortList = new ArrayList<>(students);
-        sortList.sort(Collections.reverseOrder());
-        return sortList;
+    public List<PreAcademyStudent> sort() {
+        switch (sortType) {
+            case TOTAL_DESC:
+                return sortTotalInDesc();
+            case TOTAL_ASC:
+                return sortTotalInAsc();
+            case TASKS_DESC:
+                return sort(PreAcademyStudent::getTasksPoints, true);
+            case TASKS_ASC:
+                return sort(PreAcademyStudent::getTasksPoints, false);
+            case QUIZZES_DESC:
+                return sort(PreAcademyStudent::getQuizzesPoints, true);
+            case QUIZZES_ASC:
+                return sort(PreAcademyStudent::getQuizzesPoints, false);
+            case ACTIVITY_DESC:
+                return sort(PreAcademyStudent::getLectureActivity, true);
+            case ACTIVITY_ASC:
+                return sort(PreAcademyStudent::getLectureActivity, false);
+        }
+        return students;
     }
 
+    // sort using Comparable
+    private List<PreAcademyStudent> sortTotalInDesc() {
+        students.sort(Collections.reverseOrder());
+        return students;
+    }
+
+    private List<PreAcademyStudent> sortTotalInAsc() {
+        Collections.sort(students);
+        return students;
+    }
+
+
     // sort using Comparators
-    private List<PreAcademyStudent> sortUsingNaturalOrderings() {
-        List<PreAcademyStudent> sortList = new ArrayList<>(students);
-        sortList.sort(Comparator.comparing(PreAcademyStudent::getTasksPoints)
-                .thenComparing(PreAcademyStudent::getQuizzesPoints)
-                .thenComparing(PreAcademyStudent::getLectureActivity)
-                .reversed());
-        return sortList;
+    private List<PreAcademyStudent> sort(Function<PreAcademyStudent, Integer> fieldToSortBy, boolean inDescOrder) {
+        if (inDescOrder) {
+            students.sort(Comparator.comparing(fieldToSortBy));
+        } else {
+            students.sort(Comparator.comparing(fieldToSortBy).reversed());
+        }
+        return students;
     }
 
     public static void main(String[] args) {
 
-        StudentsBinder studentsBinder = new StudentsBinder("preAcademyStudents.csv", SortType.TOTAL_DESC);
-        System.out.println(studentsBinder.sortUsingTotalPoints());
-        System.out.println(studentsBinder.sortUsingNaturalOrderings());
+        StudentsBinder studentsBinder = new StudentsBinder("preAcademyStudents.csv", TOTAL_DESC);
+
+        try (CVSWriter cvsWriter = new CVSWriter()) {
+//            cvsWriter.givenDataArray_whenConvertToCSV_thenOutputCreated();
+        }catch (Exception e) {
+            System.out.println("Something goes wrong...");
+        }
     }
 
 
