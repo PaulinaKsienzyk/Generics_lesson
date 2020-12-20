8932be6b74b089b11774c9bfcc67a3951eea6e4d
Paulina Ksienzyk <pilnicka8@gmail.com>
2020-12-20 21:34:53 +0100

    Add nrs to sorted types

--- src/main/java/comparisons/PreAcademy/SortType.java
+++ src/main/java/comparisons/PreAcademy/SortType.java
@@ -1,4 +1,23 @@
 package comparisons.PreAcademy;
 
 public enum SortType {
+
+    TOTAL_DESC(0),
+    TOTAL_ASC(1),
+    QUIZZES_DESC(2),
+    QUIZZES_ASC(3),
+    TASKS_DESC(4),
+    TASKS_ASC(5),
+    ACTIVITY_DESC(5),
+    ACTIVITY_ASC(6);
+
+    private int nr;
+
+    SortType(int nr) {
+        this.nr = nr;
+    }
+
+    public int getNr() {
+        return nr;
+    }
 }
